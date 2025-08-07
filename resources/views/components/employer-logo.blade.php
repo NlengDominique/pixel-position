2369d3a7fed017989ce4430b1e44e7f528d5b726
NlengDominique <luqnleng5@gmail.com>
2025-08-07 18:25:11 +0100

    ui(jobs): finishing with the design of my app

---
+++ resources/views/components/employer-logo.blade.php
@@ -0,0 +1,3 @@
+@props(['width'=>90])
+
+<img src="https://picsum.photos/seed/{{rand(1,1000000)}}/{{$width}}" alt="Logo du work" class="rounded-xl">
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<x-panel class="p-4 bg-white/5 rounded-xl transition-colors duration-300 flex flex-col text-center border border-transparent group hover:border-blue-800">
+    <div class="self-start text-sm">
+        Laracasts
+    </div>
+    <div class="py-8 ">
+        <h3 class="group-hover:text-blue-800 text-xl transition-colors duration-300 font-bold">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag size="small">Tag</x-tag>
+            <x-tag size="small">Tag</x-tag>
+            <x-tag size="small">Tag</x-tag>
+        </div>
+        <x-employer-logo :width="42"/>
+    </div>
+</x-panel>
